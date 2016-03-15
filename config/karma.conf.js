e300496628eff54fbad8ec2d2eb30bc1bf5044b4
Alex Baumgertner <alex.baumgertner@gmail.com>
2016-03-15 22:10:27 +0300

    Fix dependencies

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -88,7 +88,7 @@ module.exports = function(config) {
       'karma-chai',
       'karma-sinon',
       'karma-sinon-chai',
-      'karma-phantomjs-launcher',
+      'karma-phantomjs2-launcher',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-ie-launcher',
@@ -106,7 +106,7 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS2'],
 
     browserDisconnectTimeout: 10000,
 
